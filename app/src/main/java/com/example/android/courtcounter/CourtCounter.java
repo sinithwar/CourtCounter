0244f811309b6de902c9d4ac51321e1e58bb3347
sinithwar <sinithwar@yahoo.com>
2016-11-08 17:56:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/CourtCounter.java
@@ -0,0 +1,13 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourtCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_court_counter);
+    }
+}
